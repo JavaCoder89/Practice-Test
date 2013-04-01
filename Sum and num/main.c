bdeeab7f977f5b2f44f251c6ae3aaa537f7db07b
Vaughn Clayton <vaughn@vaughns-MacBook.local>
2013-04-01 16:01:22 -0500

    Initial Commit

---
+++ Sum and num/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Sum and num
+//
+//  Created by Vaughn Clayton on 4/1/13.
+//  Copyright (c) 2013 Vaughn Clayton. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
